c371cc56dcd01fc9bf4cb408351a80865d1809d2
buzzerbyte <rujyi94@hotmail.com>
2019-08-24 11:26:47 +0800

    remove syntax error

--- src/components/views/lists/screen1.js
+++ src/components/views/lists/screen1.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import {
   StyleSheet,
   Text,
@@ -10,13 +12,20 @@ import {
   SafeAreaView,
   AsyncStorage,
 } from 'react-native';
-import { Avatar, Button, Icon, Input} from 'react-native-elements';
+import {
+  Avatar,
+  Button,
+  Icon,
+  Input
+} from 'react-native-elements';
 import axios from 'axios';
-import { TextInput } from 'react-native-gesture-handler';
+import {
+  TextInput
+} from 'react-native-gesture-handler';
 // import { cacheFonts } from '../../helpers/AssetsCaching';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
-const BEARER_TOKEN = null;
+// const BEARER_TOKEN = null;
 var taskArray = [];
 export default class ListsScreen1 extends Component {
   constructor(props) {
@@ -32,136 +41,154 @@ export default class ListsScreen1 extends Component {
     this.getAllTask = this.getAllTask.bind(this);
   }
 
-  async getToken(){
-    try{
-      const userData = await AsyncStorage.getItem('userData',(err,value) => {
+  async getToken() {
+    try {
+      const userData = await AsyncStorage.getItem('userData', (err, value) => {
         // console.log("Get Token: "+JSON.parse(value));
       });
-      
+
       return userData;
-      
-    }catch(error){
+
+    } catch (error) {
       console.log('Something went wrong', error);
     }
   }
 
   async componentDidMount() {
     this.getToken().then((data) => {
-      console.log("Token: "+ BEARER_TOKEN);
+      console.log("Token: " + data);
       // BEARER_TOKEN = data;
       // console.log("Get:"+data);
-      const AuthStr = "Bearer ".concat(BEARER_TOKEN);
+      const AuthStr = "Bearer ".concat(data);
 
       // console.log(AuthStr);
-      axios.get('http://192.168.1.97/api/task', { 
-        headers: { Authorization: AuthStr },
-      })
-      .then(response => {
-        // If request is good...
-        taskArray = response.data;
-        //add a property onUpdate, and set that to false
-        taskArray = taskArray.map((data)=>{
-          var o = Object.assign({}, data);
-          o.onUpdate = false;
-          return o;
+      axios.get('http://192.168.1.97/api/task', {
+          headers: {
+            Authorization: AuthStr
+          },
+        })
+        .then(response => {
+          // If request is good...
+          taskArray = response.data;
+          //add a property onUpdate, and set that to false
+          taskArray = taskArray.map((data) => {
+            var o = Object.assign({}, data);
+            o.onUpdate = false;
+            return o;
+          });
+          this.setState({
+            tasks: taskArray
+          });
+
+        })
+        .catch((error) => {
+          console.log('error ' + error);
         });
-        this.setState({ tasks: taskArray });
-       
-      })
-      .catch((error) => {
-        console.log('error ' + error);
-      });
     });
-    
-    this.setState({ fontLoaded: true });
+
+    this.setState({
+      fontLoaded: true
+    });
   }
 
-  getAllTask(){
+  getAllTask() {
     this.getToken().then((data) => {
       // console.log("Token: "+ data);
       // console.log("Get:"+data);
-      const AuthStr = 'Bearer '.concat(data); 
+      const AuthStr = 'Bearer '.concat(data);
       // console.log(AuthStr);
-      axios.get('http://192.168.1.97/api/task', { 
-        headers: { Authorization: AuthStr },
-      })
-      .then(response => {
-        // If request is good...
-        taskArray = response.data;
-        //add a property onUpdate, and set that to false
-        taskArray = taskArray.map((data)=>{
-          var o = Object.assign({}, data);
-          o.onUpdate = false;
-          return o;
+      axios.get('http://192.168.1.97/api/task', {
+          headers: {
+            Authorization: AuthStr
+          },
+        })
+        .then(response => {
+          // If request is good...
+          taskArray = response.data;
+          //add a property onUpdate, and set that to false
+          taskArray = taskArray.map((data) => {
+            var o = Object.assign({}, data);
+            o.onUpdate = false;
+            return o;
+          });
+          this.setState({
+            tasks: taskArray
+          });
+
+        })
+        .catch((error) => {
+          console.log('error ' + error);
         });
-        this.setState({ tasks: taskArray });
-       
-      })
-      .catch((error) => {
-        console.log('error ' + error);
-      });
     });
-  
+
   }
 
-  onSubmitNewTask(newTask){
-    const AuthStr = 'Bearer '.concat(data); 
+  onSubmitNewTask(newTask) {
+    const AuthStr = 'Bearer '.concat(data);
     axios.post('http://192.168.1.97/api/task', {
-      name: newTask,
-      lastName: ''
-    },{
-      headers: { Authorization: AuthStr },
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+        name: newTask,
+        lastName: ''
+      }, {
+        headers: {
+          Authorization: AuthStr
+        },
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     this.getAllTask();
   }
 
-  onSubmitUpdateTask(updateTask, id){
-    const AuthStr = 'Bearer '.concat(BEARER_TOKEN); 
-    axios.put('http://192.168.1.97/api/task/'+id, {
-      name: updateTask,
-    },{
-      headers: { Authorization: AuthStr },
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  onSubmitUpdateTask(updateTask, id) {
+    const AuthStr = 'Bearer '.concat(BEARER_TOKEN);
+    axios.put('http://192.168.1.97/api/task/' + id, {
+        name: updateTask,
+      }, {
+        headers: {
+          Authorization: AuthStr
+        },
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     this.getAllTask();
   }
 
-  onDeleteTask(id){
-    const AuthStr = 'Bearer '.concat(BEARER_TOKEN); 
-    axios.delete('http://192.168.1.97/api/task/'+id, {
-      headers: { Authorization: AuthStr },
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  onDeleteTask(id) {
+    const AuthStr = 'Bearer '.concat(BEARER_TOKEN);
+    axios.delete('http://192.168.1.97/api/task/' + id, {
+        headers: {
+          Authorization: AuthStr
+        },
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     this.getAllTask();
   }
 
-  findTaskIndex(taskId){
-    let {tasks} = this.state;
-    for(var i = 0; i < tasks.length; i++){
-      if(tasks[i].id == taskId){
+  findTaskIndex(taskId) {
+    let {
+      tasks
+    } = this.state;
+    for (var i = 0; i < tasks.length; i++) {
+      if (tasks[i].id == taskId) {
         return i;
       }
     }
     return -1;
   }
 
-  toggleEditTask(taskId){
+  toggleEditTask(taskId) {
     var foundIndex = this.findTaskIndex(taskId);
     var newTasks = this.state.tasks;
     newTasks[foundIndex].onUpdate = !newTasks[foundIndex].onUpdate;
@@ -172,12 +199,12 @@ export default class ListsScreen1 extends Component {
   }
 
   renderValue(task) {
-    const { value, positive } = task;
+    const {value,positive} = task;
 
     if (positive) {
-      return (
-        <View
-          style={{
+      return ( 
+        <View style = {
+          {
             backgroundColor: 'rgba(220,230,218,1)',
             width: 70,
             height: 28,
@@ -186,208 +213,229 @@ export default class ListsScreen1 extends Component {
             alignItems: 'center',
             flexDirection: 'row',
             marginLeft: 10,
-          }}
-        >
-          <Icon name="md-arrow-dropup" type="ionicon" color="green" size={25} />
-          <Text
-            style={{
+          }
+        } >
+          <Icon 
+          name = "md-arrow-dropup"
+          type = "ionicon"
+          color = "green"
+          size = {25}
+          /> 
+          <Text style = {
+            {
               color: 'green',
               fontFamily: 'regular',
               fontSize: 13,
               marginLeft: 5,
-            }}
-          >
-            {value}
-          </Text>
+            }
+          } > {value} 
+          </Text> 
         </View>
       );
     } else {
       return (
         <View
-          // style={{
-          //   backgroundColor: 'rgba(244,230,224,1)',
-          //   width: 70,
-          //   height: 28,
-          //   borderRadius: 5,
-          //   justifyContent: 'center',
-          //   alignItems: 'center',
-          //   flexDirection: 'row',
-          //   marginLeft: 10,
-          // }}
+        // style={{
+        //   backgroundColor: 'rgba(244,230,224,1)',
+        //   width: 70,
+        //   height: 28,
+        //   borderRadius: 5,
+        //   justifyContent: 'center',
+        //   alignItems: 'center',
+        //   flexDirection: 'row',
+        //   marginLeft: 10,
+        // }}
         >
-          {/* <Icon name="md-arrow-dropdown" type="ionicon" color="red" size={25} />
-          <Text
-            style={{
-              color: 'red',
-              fontFamily: 'regular',
-              fontSize: 13,
-              marginLeft: 5,
-            }}
-          >
-            {value}
-          </Text> */}
+        {
+          /* <Icon name="md-arrow-dropdown" type="ionicon" color="red" size={25} />
+                    <Text
+                      style={{
+                        color: 'red',
+                        fontFamily: 'regular',
+                        fontSize: 13,
+                        marginLeft: 5,
+                      }}
+                    >
+                      {value}
+                    </Text> */
+        } 
         </View>
       );
     }
   }
 
   renderCard(task, index) {
-    const {id, name , description } = task;
-    
-    return (
-      <View
-        key={index}
-        style={{
-          height: 60,
-          marginHorizontal: 10,
-          marginTop: 10,
-          backgroundColor: 'white',
-          borderRadius: 5,
-          alignItems: 'center',
-          flexDirection: 'row',
-        }}
-      >
-        <View style={{ flex: 2, flexDirection: 'row', alignItems: 'center' }}>
-          
-          <View style={{ marginLeft: 15 }}>
-            {/* <Avatar
-              small
-              rounded
-              source={{
-                uri: avatar,
-              }}
-              activeOpacity={0.7}
-            /> */}
-          </View>
-          {task.onUpdate ? (
-            <Input
-              style={{
-                fontFamily: 'regular',
-                fontSize: 15,
-                marginLeft: 10,
-                color: 'gray',
-              }}
-              onChangeText={(updateTask) => this.setState({updateTask})}
-              autoFocus
-              //onBlur={() => this.toggleEditTask(task.id)}          
-            >
-              {name}
-            </Input>
-          ) : (
-            <Text
-              style={{
-                fontFamily: 'regular',
-                fontSize: 15,
-                marginLeft: 10,
-                color: 'gray',
-              }}
-              onPress={() => this.toggleEditTask(task.id)}               
-            >
-              {name}
-            </Text>
-          )}
-          <Text
-            style={{
+    const {
+      id,
+      name,
+      description
+    } = task;
+
+    return ( 
+      <View 
+        key = {index}
+        style = {{
+            height: 60,
+            marginHorizontal: 10,
+            marginTop: 10,
+            backgroundColor: 'white',
+            borderRadius: 5,
+            alignItems: 'center',
+            flexDirection: 'row',
+        }} >
+        <View style = {
+          {
+            flex: 2,
+            flexDirection: 'row',
+            alignItems: 'center'
+          }
+        } >
+
+        <View style = {{
+            marginLeft: 15
+        }}> 
+        </View> 
+        {task.onUpdate ? ( 
+          <Input style = {
+            {
               fontFamily: 'regular',
               fontSize: 15,
               marginLeft: 10,
               color: 'gray',
-            }}
-          >
-            {/* {description} */}
-          </Text>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'center',
-            marginRight: 10,
-          }}
-        >
-          {this.renderValue(task)}
-          <View
-            style={{
-              backgroundColor: 'rgba(222,222,222,1)',
-              width: 35,
-              height: 28,
-              borderRadius: 5,
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginHorizontal: 10,
-            }}
-          >
-          {task.onUpdate ? (
-            <Icon
-              name="plus"
-              type="font-awesome"
-              color="#86939e"
-              size={20}
-              onPress={() => this.onSubmitUpdateTask(this.state.updateTask, task.id)} 
-            />
-          ):(
-            <Icon 
-              name="md-trash" 
-              type="ionicon" 
-              color="gray" 
-              size={20}  
-              onPress={() => this.onDeleteTask(task.id)} 
-            />
-          )}
-            
-          </View>
-        </View>
-      </View>
-    );
-  }
-
-  renderListCards() {
-    
-    return _.map(this.state.tasks, (task, index) => {
-      return this.renderCard(task, index);
-    });
-  }
-
-  render() {
-    
-    return (
-      <View>
-        {this.state.fontLoaded ? (
-          <SafeAreaView
-            style={{ flex: 1, backgroundColor: 'rgba(241,240,241,1)' }}
-          >
-            <View style={styles.statusBar} />
-            <View style={styles.navBar}>
-              <Text style={styles.nameHeader}>Tasks</Text>
-            </View>
-            <ScrollView style={{ flex: 1, marginBottom: 20 }}>
-              
-              {this.renderListCards()}
-            </ScrollView>
-            <View style={{ alignItems: 'center', marginBottom: 16 }}>
-            <Input
-              rightIcon={
-                <Icon
-                  name="plus"
-                  type="font-awesome"
-                  color="#86939e"
-                  size={25}
-                  onPress={() => this.onSubmitNewTask(this.state.newTask)} 
-                />
-              }
-              onChangeText={(newTask) => this.setState({newTask})}
-              leftIconContainerStyle={{ marginLeft: 0, marginRight: 10 }}
-              containerStyle={styles.inputContainerStyle}
-              placeholder="New task"
-            />
-            </View>
-          </SafeAreaView>
+            }
+          }
+          onChangeText = {(updateTask) => this.setState({updateTask})}
+          autoFocus>
           
-        ) : (
-          <Text>Loading...</Text>
-        )}
+          {name}</Input>
+        ) : ( 
+          <Text style = {
+            {
+              fontFamily: 'regular',
+              fontSize: 15,
+              marginLeft: 10,
+              color: 'gray',
+            }
+          }
+          onPress = {() => this.toggleEditTask(task.id)} > 
+          {name} 
+          </Text>
+        )
+      } 
+        <Text style = {
+          {
+            fontFamily: 'regular',
+            fontSize: 15,
+            marginLeft: 10,
+            color: 'gray',
+          }
+        } > {/* {description} */} 
+        </Text> 
+      </View> 
+      <View style = {{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          marginRight: 10,
+        }} > 
+        {this.renderValue(task)} 
+        <View style = {
+          {
+            backgroundColor: 'rgba(222,222,222,1)',
+            width: 35,
+            height: 28,
+            borderRadius: 5,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginHorizontal: 10,
+          }
+        } > 
+        {
+          task.onUpdate ? ( 
+            <Icon 
+              name = "plus"
+              type = "font-awesome"
+              color = "#86939e"
+              size = {20}
+              onPress = {() => this.onSubmitUpdateTask(this.state.updateTask, task.id)}
+            />
+          ) : ( 
+            <Icon 
+              name = "md-trash"
+              type = "ionicon"
+              color = "gray"
+              size = {20}
+              onPress = {() => this.onDeleteTask(task.id)}
+            />
+          )
+        }
+
+        </View> 
       </View>
-    );
-  }
+    </View>
+  );
+}
+
+renderListCards() {
+
+  return _.map(this.state.tasks, (task, index) => {
+    return this.renderCard(task, index);
+  });
+}
+
+render() {
+
+  return ( 
+    <View> 
+    {this.state.fontLoaded ? ( 
+        <SafeAreaView style = {{
+            flex: 1,
+            backgroundColor: 'rgba(241,240,241,1)'
+          }}>
+        <View style = {styles.statusBar}/> 
+        <View style = {styles.navBar}>
+        <Text style = {styles.nameHeader}> Tasks </Text> 
+        </View> 
+          <ScrollView style = {{
+              flex: 1,
+              marginBottom: 20
+            }}>
+
+          {this.renderListCards()} 
+          </ScrollView> 
+        <View style = {
+          {
+            alignItems: 'center',
+            marginBottom: 16
+          }
+        } >
+        <Input rightIcon = {
+          <Icon
+          name = "plus"
+          type = "font-awesome"
+          color = "#86939e"
+          size = {25}
+          onPress = {() => this.onSubmitNewTask(this.state.newTask)}
+          />
+        }
+        onChangeText = {(newTask) => this.setState({newTask})}
+        leftIconContainerStyle = {{
+            marginLeft: 0,
+            marginRight: 10
+        }}
+        containerStyle = {
+          styles.inputContainerStyle
+        }
+        placeholder = "New task" />
+        </View> 
+        </SafeAreaView>
+
+      ) : ( 
+        <Text> Loading... </Text>
+      )
+    } 
+    </View>
+  );
+}
 }
 
 const styles = StyleSheet.create({
